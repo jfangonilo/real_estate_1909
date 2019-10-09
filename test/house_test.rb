cd9b09c7b569e4ba264800e552be7cc9d6a631dd
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-09 09:24:08 -0600

    Add add_room test

--- test/house_test.rb
+++ test/house_test.rb
@@ -5,7 +5,8 @@ require './lib/house'
 
 class HouseTest < Minitest::Test
   def setup
-    @room = Room.new(:bedroom, 10, 13)
+    @room_1 = Room.new(:bedroom, 10, 13)
+    @room_2 = Room.new(:bedroom, 11, 15)
     @house = House.new("$400000", "123 sugar lane")
   end
 
@@ -19,5 +20,11 @@ class HouseTest < Minitest::Test
 
   def test_it_has_an_address
     assert_equal "123 sugar lane", @house.address
+  end
+
+  def test_add_room
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    assert_equal [@room_1, @room_2], @house.rooms
   end
 end
