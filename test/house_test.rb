b347229fcadca51c971a52030a3cf4c745f3d6cb
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-09 09:21:08 -0600

    Add test house exists and has price/address

---
+++ test/house_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+require './lib/house'
+
+class HouseTest < Minitest::Test
+  def setup
+    @room = Room.new(:bedroom, 10, 13)
+    @house = House.new("$400000", "123 sugar lane")
+  end
+
+  def test_it_exists
+    assert_instance_of House, @house
+  end
+
+  def test_it_has_a_price
+    assert_equal "$400000", @house.price
+  end
+
+  def test_it_has_an_address
+    assert_equal "123 sugar lane", @house.address
+  end
+end
