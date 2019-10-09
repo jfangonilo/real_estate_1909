91fb2e2e62d30cd1518bbece9aa7dd323ab4771d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-09 09:31:34 -0600

    Add test for rooms_from_category

--- test/house_test.rb
+++ test/house_test.rb
@@ -7,6 +7,8 @@ class HouseTest < Minitest::Test
   def setup
     @room_1 = Room.new(:bedroom, 10, 13)
     @room_2 = Room.new(:bedroom, 11, 15)
+    @room_3 = Room.new(:living_room, 25, 15)
+    @room_4 = Room.new(:basement, 30, 41)
     @house = House.new("$400000", "123 sugar lane")
   end
 
@@ -28,5 +30,15 @@ class HouseTest < Minitest::Test
     assert_equal [@room_1], @house.rooms
     @house.add_room(@room_2)
     assert_equal [@room_1, @room_2], @house.rooms
+  end
+
+  def test_rooms_from_category
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+
+    assert_equal [@room_1, @room_2], @house.rooms_from_category(:bedroom)
+    assert_equal [@room_4], @house.rooms_from_category(:basement)
   end
 end
