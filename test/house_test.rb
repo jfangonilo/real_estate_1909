095c74c5eb6def3d470537835e94a199e40c7ac1
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-09 09:26:27 -0600

    Add additional rooms and initial state test to add_room test

--- test/house_test.rb
+++ test/house_test.rb
@@ -23,7 +23,9 @@ class HouseTest < Minitest::Test
   end
 
   def test_add_room
+    assert_equal [], @house.rooms
     @house.add_room(@room_1)
+    assert_equal [@room_1], @house.rooms
     @house.add_room(@room_2)
     assert_equal [@room_1, @room_2], @house.rooms
   end
